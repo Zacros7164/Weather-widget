7af83a9465d09530ab181cac19ce4ca2ab0878ae
Zac Crosby <Zacros7164@gmail.com>
2018-12-19 15:08:18 -0500

    added JSON display

--- scripts.js
+++ scripts.js
@@ -1,2 +1,29 @@
 // console.log("test")
-// console.log(appKey)
+$('#weather-form').submit((e)=>{
+    e.preventDefault();
+    // console.log("user submit");
+    const zip= $('.zip-code').val()
+    $('.zip-code').val('')
+    console.log(zip)
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=imperial`;
+
+    // we got the zip code 
+    // we built the URL 
+    // now go get the JSON
+
+    $.getJSON(weatherUrl,(weatherData)=>{
+        console.log(weatherData)
+        const currTemp = weatherData.main.temp;
+        const temps = {
+            curr: parseInt(weatherData.main.temp),
+            max: weatherData.main.temp_max,
+            min: weatherData.main.temp_min
+        }
+        const newHTML = `<img src="https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png" />
+        <div>The temp in ${weatherData.name} is currently ${temps.curr} &deg;</div>
+        `
+        $('.weather-data').html(newHTML);
+
+
+    }) //getJSON
+}) // submit form
